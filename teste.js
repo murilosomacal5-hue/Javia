cc63855d498824d03283844fdebfa4c7b655c75f
Murilo <murilosomacal5@gmail.com>
2026-05-04 20:27:22 -0300

    comados01

--- teste.js
+++ teste.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+//console.log("Hello World!")
+
+let idade = 10;
+idade = 20;
+
+console.log(idade);
+
+console.log(typeof idade); //string
+console.log(typeof 'abc'); // 'string'
+console.log(typeof 10);// 'number'
+console.log(typeof true);// 'boolean'
+console.log(typeof undefined);// 'undefine'
+console.log(typeof null);// 'object' (bug)
+console.log(typeof 123n);// 'bigint'
+console.log(typeof Symbol('id'));// 'Symbol'
+
+//verificação de tipos
+let valor = 'texto';
+if (typeof valor == 'string') {
+    console.log('é uma string');
+}
